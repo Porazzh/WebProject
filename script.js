112d57e79491f034e04d67269160024314c4b3e9
Porazzh <frifun35@gmail.com>
2023-11-05 22:58:47 +0100

    1st commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav(){
+    document.querySelector("#mySideBar").style.width = "250px";
+    document.querySelector("#chowac").style.marginLeft = "260px"
+}
+function closeNav(){
+    document.querySelector("#mySideBar").style.width = "0";
+    document.querySelector("#chowac").style.marginLeft = "0"
+}
