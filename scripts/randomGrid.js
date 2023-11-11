34cad921d940b69a56d1eefe5a894de38cb5b0ad
Porazzh <frifun35@gmail.com>
2023-11-12 00:47:15 +0100

    12.11
    Grid added
    sideBar shrink to 64px

---
+++ scripts/randomGrid.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded",()=>{
+    let btn = document.querySelector("#jd");
+        let losLiczba = Math.round(Math.random())%2;
+        let grid = document.querySelector("#gridOne");
+        switch (losLiczba) {
+            case 0 :
+            grid.classList.add("gridParrentFirst")
+            grid.classList.remove("gridParrentSecond")
+            break
+            default :
+            grid.classList.add("gridParrentSecond")
+            grid.classList.remove("gridParrentFirst") 
+        }
+})
---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", () =>{
+    sideBar = document.querySelector("#mySideBar");
+    openBtn = document.querySelector("#openSbBtn");
+    closeBtn = document.querySelector("#closeSbBtn");
+    openBtn.addEventListener("click", () =>{
+        sideBar.classList.add("openedSideBar")
+        sideBar.classList.remove("closedSideBar")
+        document.querySelector("#chowac").style.marginLeft = "260px"
+    })
+    closeBtn.addEventListener("click", () =>{
+        sideBar.classList.add("closedSideBar")
+        sideBar.classList.remove("openedSideBar")
+        document.querySelector("#chowac").style.marginLeft = "74px"
+    })
+})
